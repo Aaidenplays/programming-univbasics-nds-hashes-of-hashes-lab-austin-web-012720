5a910e4713675d3dc46dea8a88e482193da4438a
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-02 15:51:51 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,45 +1,68 @@
+require 'pry'
 # Build a nested HoH of the classification of biological life on Earth based
 # on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
 # that HoH as the return value for the method "naming_system."
 #
 # The tests will guide your construction.
 #
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+PORTION_1 = {
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class"
+    }
+  }
+}
+
+PORTION_2 = {
+  label: "Order"
+}
+
+PORTION_3 = {
+  label: "Family",
+  sub_category: {
+    label: "Genus",
+  }
+}
+
+PORTION_4 = {
+  label: "Species",
+  sub_category: nil
+}
 
 def naming_system
   # Remember:
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
+  #   Kingdom
+  #   Phylum
+  #   Class
+  #   Order
+  #   Family
+  #   Genus
+  #   Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
+  
+  species ={  
+  portion1: {label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Order"
+    }
+  }},  
+  portion2: {label: "Order"},
+  portion3: {label: "Family",
+  sub_category: {
+    label: "Genus"
+  }},
+  portion4: {label: "Species",
+  sub_category: nil
+  }}
 
+#species = {PORTION_1, PORTION_2, PORTION_3, PORTION_4}
+  
+  #species[:portion1][:sub_category][:sub_category][:label] = "Order"
+  #binding.pry
+  species
 end
